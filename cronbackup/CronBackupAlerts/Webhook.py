020150ee6ad129166bd2582a2e3d17958c15a5cf
BMarcin <m1.borzymowski@gmail.com>
2022-10-11 19:40:09 +0200

    test lint

--- cronbackup/CronBackupAlerts/Webhook.py
+++ cronbackup/CronBackupAlerts/Webhook.py
@@ -1,11 +1,13 @@
 from cronbackup.CronBackupAlerts.CronBackupAlert import CronBackupAlert
 import requests
 
+
 class Webhook(CronBackupAlert):
     def __init__(self, config: dict, *args, **kwargs):
         super().__init__(config, *args, **kwargs)
         self.config = config
-        assert set(self.config.keys()) == {"url"}, "Invalid config keys for Webhook"
+        assert set(self.config.keys()) == {"url"}, \
+            "Invalid config keys for Webhook"
         self.url = self.config["url"]
 
     def send_alert(self, service: str, message: str, *args, **kwargs):
